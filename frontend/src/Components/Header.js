fb7b72c59a80cac68bc39a9f0efcd50bce382b99
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-05-04 18:34:14 +0530

    Created Counter button

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
+  let [val,setVal] = useState(0);
+  let valHandler = () =>{
+    setVal(val = val + 1);
+  }
   return (
     <div>
-      
+      {val}
+      <button onClick={valHandler}>Click</button>
     </div>
   )
 }
