550ad62d62cb0205e3f9953d2de0a7a113f82ca0
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-05-04 20:58:11 +0530

    Fetch API

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,17 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
 const Header = () => {
   let [val,setVal] = useState(0);
+   const [posts, setPosts] = useState([]);
+
   let valHandler = () =>{
     setVal((previousVal) => previousVal + 1);
   }
 
+  useEffect(()=>{
+    document.title = `Clicked ${val}`
+  })
+
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(res => setPosts(res.data))
+    .catch(err=>console.log(err))
+  },[])
 
   return (
     <div>
       {val}
       <button onClick={valHandler}>Click</button>
+      <ul>
+      {posts.map(post=>(
+        <li key={post.id}>{post.title}</li>
+        ))}</ul>
+     
     </div>
   )
 }
